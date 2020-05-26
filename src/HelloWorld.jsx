d2b1c246d20ca984056fdda6a2ad9ffcec13e772
collinskoech11 <collinskoechck34@gmail.com>
2020-05-26 22:40:35 +0300

    first commit

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,19 @@
+import React,{Component } from 'react';
+
+
+
+
+
+class HelloWorld extends Component {
+  render(){
+    return(
+      <div>
+      <h2>Hello<en>{this.props.name}</en> </h2>
+      </div>
+    );
+  }
+  }
+
+
+
+export default HelloWorld;
